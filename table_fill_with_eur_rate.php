1056bc5dd324b81831b81da5e65bea5f0255659d
Danila Popov <Danila Popov>
2016-06-26 21:36:38 +1000

    test google charts json import added

---
+++ table_fill_with_eur_rate.php
@@ -0,0 +1,31 @@
+<?php
+require 'db_pass.php';
+$db_name = 'test';
+
+
+$conn = mysqli_connect($servername, $db_root, $db_pass, $db_name);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+
+// $sql = "insert into eur_rates values(curdate() + interval 2 day, 74.2222)";
+
+
+$XML=simplexml_load_file("http://www.ecb.europa.eu/stats/exchange/eurofxref/html/rub.xml");
+ 
+
+
+foreach ($XML->DataSet->Series->Obs as $rate) {
+    $sql = "insert into eur_rates values('" . $rate["TIME_PERIOD"] . "', ". $rate["OBS_VALUE"] . ")";
+    //echo $sql;
+    if ($conn->query($sql) === true) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    //echo $rate["TIME_PERIOD"].' '.'1&euro;='.' '.$rate["OBS_VALUE"].'<br/>';
+}
+
+$conn->close();
