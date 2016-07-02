ee8df94589b5a94d59f511b3cac490b70121468d
danvop <danvop@gmail.com>
2016-07-02 13:26:58 +1000

    made with data from array

--- Google_charts_json_test/getData.php
+++ Google_charts_json_test/getData.php
@@ -1,34 +1,60 @@
 <?php
 
-$table = [
-['test1', 2],
-['test2', 1],
-['test3', 3],
-];
+$get = 'get_get';
+
+if (isset($_GET['get'])) {
+        $get = strtolower(trim(strip_tags($_GET['get'])));
+}
+
+
+$table = array(
+    'test' => 2,
+    'test1' => 5,
+    'Тест' => 2,
+    'test3' => 1,
+    'test5' => 3,
+    );
+
 
 $json = array();
 $json['cols'] = array(
         array('label' => 'Topping', 'type' => 'string'),
-        array('label' => 'Slices', 'type' => 'number')
+        array('label' => 'Slices', 'type' => 'number'),
+
     );
 
-$rows = array();
+
+foreach ($table as $key => $item) {
+        $temp = [];
+        $temp[] = array('v' => $key);
+        $temp[] = array('v' => $item);
+        $rows[] = array('c' => $temp);
+}
+
+/*$rows = array();
 
 $temp = array();
-$temp[]=array('v' => 'test1');
+$temp[]=array('v' => $get); //тестирую передачу другого параметра
 $temp[]=array('v' => 3);
-
 $rows[0] = array ('c' => $temp);
 
 
 $temp = array();
 $temp[]=array('v' => 'test2');
 $temp[]=array('v' => 2);
-
 $rows[1] = array ('c' => $temp);
+
+$temp = array();
+$temp[]=array('v' => 'test3');
+$temp[]=array('v' => 2);
+
+$rows[2] = array ('c' => $temp);*/
+
 
 $json['rows'] = $rows;
 
-$string = file_get_contents("sampleData.json");
 
+
+
+//$string = file_get_contents("sampleData.json");
 echo json_encode($json);
