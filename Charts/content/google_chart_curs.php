2b9aa4a704736a4c8a6fbeb77127bcef760ff3c1
danvop <danvop@gmail.com>
2016-07-02 16:46:13 +1000

    google chart added real eur rub ecb rates

--- Charts/content/google_chart_curs.php
+++ Charts/content/google_chart_curs.php
@@ -1,3 +1,46 @@
+<?php
+
+require '../db_pass.php';
+$db_name = 'test';
+//$weeks = 1;
+
+
+$conn = mysqli_connect($servername, $db_root, $db_pass, $db_name);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+// echo "Connected successfully";
+
+$sql = "SELECT *  
+        FROM eur_rates 
+        WHERE date between '2015-06-17' and '2016-06-24'";
+$result = $conn->query($sql);
+
+
+
+$json = array();
+$json['cols'] = array(
+        array('label' => 'date', 'type' => 'string'),
+        array('label' => 'EUR RUB ECB', 'type' => 'number')
+        // array('label' => 'EUR CBR', 'type' => 'number')
+    );
+
+
+while ($row = $result->fetch_assoc()) {
+    $temp = [];
+    $temp[] = array('v' => $row['date']);
+    $temp[] = array('v' => $row['rate']);
+    $rows[] = array('c' => $temp);
+}
+
+
+$json['rows'] = $rows;
+
+//echo json_encode($json);
+
+?>
+
 <html>
 <head>
   <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
@@ -5,63 +48,14 @@
       google.charts.load('current', {'packages':['line']});
       google.charts.setOnLoadCallback(drawChart);
 
-
-      var chart_data = ([
-        ['2016-06-19',  37.8],
-        ['2016-06-20',  30.9],
-        ['2016-06-21',  25.4],
-        ['2016-06-22',  1],
-        ['2016-06-23',  11.9],
-        ['2016-06-24',   8.8],
-        ['2016-06-25',   7.6],
-        ['2016-06-26',  1],
-        ['2016-06-19',  37.8],
-        ['2016-06-20',  30.9],
-        ['2016-06-21',  25.4],
-        ['2016-06-22',  1],
-        ['2016-06-23',  11.9],
-        ['2016-06-24',   8.8],
-        ['2016-06-25',   7.6],
-        ['2016-06-26',  1]
-      ]);
-
-      /*var chart_data = ([
-        [2016-06-19,  37.8],
-        [2016-06-20,  30.9],
-        [2016-06-21,  25.4],
-        [2016-06-22,  1],
-        [2016-06-23,  11.9],
-        [2016-06-24,   8.8],
-        [2016-06-25,   7.6],
-        [2016-06-26,  1]
-      ]);*/
-
-      // var chart_data = ([
-      //   [1,  37.8, 80.8, 41.8],
-      //   [2,  30.9, 69.5, 32.4],
-      //   [3,  25.4,   57, 25.7],
-      //   [4,  1, 1, 1],
-      //   [5,  11.9, 2, 2],
-      //   [6,   8.8, 3,  3],
-      //   [7,   7.6, 12.3,  9.6],
-      //   [8,  1, 29.2,1],
-      //   [9,  16.9, 42.9, 14.8],
-      //   [10, 12.8, 30.9, 11.6],
-      //   [11,  5.3,  7.9,  4.7],
-      //   [12,  6.6,  8.4,  5.2],
-      //   [13,  4.8,  6.3,  3.6],
-      //   [14,  4.2,  6.2,  3.4]
-      // ]);
     function drawChart() {
 
-      var data = new google.visualization.DataTable();
-      data.addColumn('string', 'Day');
-      data.addColumn('number', 'Guardians of the Galaxy');
-      // data.addColumn('number', 'The Avengers');
-      // data.addColumn('number', 'Transformers: Age of Extinction');
-
-      data.addRows(chart_data);
-
+      var data = new google.visualization.DataTable(<?=json_encode($json)?>);
+      
+      var formatter = new google.visualization.NumberFormat(
+      {pattern: '###.####'});
+        formatter.format(data, 1);    
+      
       var options = {
         chart: {
           title: 'Box Office Earnings in First Two Weeks of Opening',
@@ -69,7 +63,10 @@
         },
         width: 900,
         height: 500,
-        
+
+        //selectionMode: 'multiple',
+        tooltip: {},
+    
         
       };
 
