77898d6850ae1f4c3963542ce1c2a7205c0c679a
danvop <danvop@gmail.com>
2017-03-08 15:11:39 +1000

    finish

--- views/partials/part.content.php
+++ views/partials/part.content.php
@@ -1,10 +1,11 @@
 
-
+<p>Разница курса Евро по данным Европейского ЦБ и Центробанка РФ</p>
 <!-- Contextual button for informational alert messages -->
 <a href="/?days=14"><button type="button" class="btn btn-info">2 недели</button></a>
 <a href="/?days=30"><button type="button" class="btn btn-info">1 месяц</button></a>
 <a href="/?days=90"><button type="button" class="btn btn-info">3 месяца</button></a>
 <a href="/?days=365"><button type="button" class="btn btn-info">Год</button></a>
+<p></p>
 <canvas id="graph" height="200"></canvas>
  
 <?php
---
+++ views/partials/part.nav.php
@@ -0,0 +1,18 @@
+<div class="container">
+  <div class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills float-right">
+            <li class="nav-item">
+<!--               <a class="nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact</a> -->
+            </li>
+          </ul>
+        </nav>
+        <a href="/"><h3 class="text-muted">Курс ЕВРО</h3></a>
+      </div> 
+</div>
