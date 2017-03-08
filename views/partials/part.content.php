b38f39973c1b004494945f46065e870a04cedb6b
danvop <danvop@gmail.com>
2017-03-08 15:24:12 +1000

    fixes

--- views/partials/part.content.php
+++ views/partials/part.content.php
@@ -1,12 +1,12 @@
 
 <p>Разница курса Евро по данным Европейского ЦБ и Центробанка РФ</p>
 <!-- Contextual button for informational alert messages -->
-<a href="/?days=14"><button type="button" class="btn btn-info">2 недели</button></a>
-<a href="/?days=30"><button type="button" class="btn btn-info">1 месяц</button></a>
-<a href="/?days=90"><button type="button" class="btn btn-info">3 месяца</button></a>
+<a href="/?days=14"><button type="button" class="btn btn-info">2 нед</button></a>
+<a href="/?days=30"><button type="button" class="btn btn-info">1 мес</button></a>
+<a href="/?days=90"><button type="button" class="btn btn-info">3 мес</button></a>
 <a href="/?days=365"><button type="button" class="btn btn-info">Год</button></a>
 <p></p>
-<canvas id="graph" height="200"></canvas>
+<canvas id="graph" height="250"></canvas>
  
 <?php
 
